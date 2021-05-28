d854391a73f77960fca40d56e9a615dc2ab492a6
kuba <info@weblead.pl>
2021-05-28 15:32:13 +0200

    aut

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-echo 'kubcio';
+echo 'kubciofdf';
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
