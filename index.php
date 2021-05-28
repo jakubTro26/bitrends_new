10b1db7b97d07fb8bb3cd7ad08221a32285b7d6b
kuba <info@weblead.pl>
2021-05-28 16:31:58 +0200

    aut

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-echo 'mamciafffdfa';
+echo 'mamciafffdfdsfgdfa';
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
