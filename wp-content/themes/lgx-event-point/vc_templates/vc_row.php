2d34cadd35c4ee9b4dbb103fac6c8ddc8cf526ed
kuba <info@weblead.pl>
2021-05-29 10:57:55 +0200

    po

--- wp-content/themes/lgx-event-point/vc_templates/vc_row.php
+++ wp-content/themes/lgx-event-point/vc_templates/vc_row.php
@@ -113,8 +113,7 @@ if ( ! $parallax && $has_video_bg ) {
 $css_class = preg_replace( '/\s+/', ' ', apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, implode( ' ', array_filter( $css_classes ) ), $this->settings['base'], $atts ) );
 $wrapper_attributes[] = 'class="' . esc_attr( trim( $css_class ) ) . '"';
 
-echo 'content123' ;
-var_dump($content);
+var_dump(debug_backtrace());
 
 $output .= '<div '. implode( ' ', $wrapper_attributes ) . '>';
 $output .= $full_top;
