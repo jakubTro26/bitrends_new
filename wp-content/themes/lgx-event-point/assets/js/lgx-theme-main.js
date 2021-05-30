a53b933d5df82c680f41613021f4adedc44d00c6
kuba <info@weblead.pl>
2021-05-30 12:25:23 +0200

    po

--- wp-content/themes/lgx-event-point/assets/js/lgx-theme-main.js
+++ wp-content/themes/lgx-event-point/assets/js/lgx-theme-main.js
@@ -5,6 +5,7 @@
 
     $(document).ready(function() {
         console.log(lgxScript);
+        console.log(lgxScript);
             // gallery slider
         $(".slider-container").ikSlider({
           speed: 500
