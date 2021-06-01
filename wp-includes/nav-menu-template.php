910a7702671ca5f0d80cb07ceea242e3640e8275
kuba <info@weblead.pl>
2021-06-01 18:21:28 +0200

    po

--- wp-includes/nav-menu-template.php
+++ wp-includes/nav-menu-template.php
@@ -221,12 +221,15 @@ function wp_nav_menu( $args = array() ) {
 	$sorted_menu_items = apply_filters( 'wp_nav_menu_objects', $sorted_menu_items, $args );
 
 
-	echo 'items';
-	var_dump($items);
+	
 
 
 	$items .= walk_nav_menu_tree( $sorted_menu_items, $args->depth, $args );
 	unset( $sorted_menu_items );
+
+
+	echo 'items';
+	var_dump($items);
 
 	// Attributes.
 	if ( ! empty( $args->menu_id ) ) {
