25481f4799152cc7f2f1f67c943961cb8681cb4d
kuba <info@weblead.pl>
2021-06-01 18:08:16 +0200

    po

--- wp-includes/nav-menu-template.php
+++ wp-includes/nav-menu-template.php
@@ -264,7 +264,7 @@ function wp_nav_menu( $args = array() ) {
 	if ( empty( $items ) ) {
 		return false;
 	}
-	
+	echo 'navba';
 	
 	$nav_menu .= sprintf( $args->items_wrap, esc_attr( $wrap_id ), esc_attr( $wrap_class ), $items );
 	unset( $items );
